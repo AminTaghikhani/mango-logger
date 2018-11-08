aac3bdcc7ae6b0092ac842740775d745ff31f87a
Amin Taghikhani <amin76taghikhani@gmail.com>
2018-11-08 16:53:08 +0330

    Separate logs

--- test/index.js
+++ test/index.js
@@ -1,3 +1,15 @@
-const {LOGGER} = require('../index')
+const {
+    LOGGER
+} = require('../index')
 
-LOGGER.toFile('test','error','asd')
+try {
+    throw new RangeError('out of range')
+} catch (error) {
+    LOGGER.errorToFile('test', error)
+    LOGGER.toFile('test', 'sdf', error)
+}
+
+LOGGER.objToFile('test', 'error', {
+    a: 123,
+    b: 'asd'
+})
