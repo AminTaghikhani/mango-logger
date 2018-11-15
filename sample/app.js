575932f8fd5c92590502b0e5130c15832d3dbb13
Amin Taghikhani <amin76taghikhani@gmail.com>
2018-11-15 22:13:51 +0330

    Update sample

--- sample/app.js
+++ sample/app.js
@@ -2,7 +2,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var {LOGGER} = require('../');
+var {
+    LOGGER
+} = require('../');
 var app = express();
 app.use(logger('dev'));
 app.use(express.json());
@@ -11,7 +13,9 @@ app.use(express.urlencoded({
 }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(LOGGER.request())
+app.use(LOGGER.request({
+    saveFormat: 'date'
+}))
 app.get('/', (req, res) => res.send('hi'))
 
 module.exports = app;
